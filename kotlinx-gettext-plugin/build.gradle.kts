2eee6b99d605605e9aabcfefdf369651debbfd6f
Victor Kropp <victor.kropp@jetbrains.com>
2022-04-27 09:16:20 +0200

    prepare Maven Central publishing

--- kotlinx-gettext-plugin/build.gradle.kts
+++ kotlinx-gettext-plugin/build.gradle.kts
@@ -1,9 +1,11 @@
+import java.net.URI
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
     `java-library`
     `maven-publish`
+    signing
     id("com.google.devtools.ksp")
 }
 
@@ -27,6 +29,11 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
 publishing {
     publications {
         create<MavenPublication>("kotlinx-gettext") {
@@ -34,6 +41,45 @@ publishing {
             artifactId = project.name
             version = project.version.toString()
             from(components["java"])
+
+            pom {
+                name.set("Kotlinx Gettext")
+                description.set("Kotlin Compiler plugin to extract strings for i18n in Gettext format")
+                url.set("https://github.com/kropp/kotlinx-gettext")
+
+                scm {
+                    connection.set("scm:git:git://github.com/kropp/kotlinx-gettext")
+                    developerConnection.set("scm:git:git://github.com/kropp/kotlinx-gettext")
+                    url.set("https://github.com/kropp/kotlinx-gettext/tree/main")
+                }
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("kropp")
+                        name.set("Victor Kropp")
+                        email.set("victor@kropp.name")
+                    }
+                }
+            }
         }
     }
+
+    repositories {
+        maven {
+            url = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials(PasswordCredentials::class.java)
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["kotlinx-gettext"])
 }
--- kotlinx-gettext/build.gradle.kts
+++ kotlinx-gettext/build.gradle.kts
@@ -1,9 +1,11 @@
+import java.net.URI
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
     `java-library`
     `maven-publish`
+    signing
 }
 
 dependencies {
@@ -21,6 +23,7 @@ tasks.withType<KotlinCompile> {
 }
 
 java {
+    withJavadocJar()
     withSourcesJar()
 }
 
@@ -31,6 +34,45 @@ publishing {
             artifactId = project.name
             version = project.version.toString()
             from(components["java"])
+
+            pom {
+                name.set("Kotlinx Gettext")
+                description.set("Kotlin Gettext i18n library")
+                url.set("https://github.com/kropp/kotlinx-gettext")
+
+                scm {
+                    connection.set("scm:git:git://github.com/kropp/kotlinx-gettext")
+                    developerConnection.set("scm:git:git://github.com/kropp/kotlinx-gettext")
+                    url.set("https://github.com/kropp/kotlinx-gettext/tree/main")
+                }
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("kropp")
+                        name.set("Victor Kropp")
+                        email.set("victor@kropp.name")
+                    }
+                }
+            }
         }
     }
+
+    repositories {
+        maven {
+            url = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials(PasswordCredentials::class.java)
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["kotlinx-gettext"])
 }
