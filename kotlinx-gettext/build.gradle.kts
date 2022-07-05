689def2cd753150a2849dfe95f866a9a939885bc
Victor Kropp <victor.kropp@jetbrains.com>
2022-07-05 11:41:29 +0200

    setup multiplatform plugin publishing

--- kotlinx-gettext/build.gradle.kts
+++ kotlinx-gettext/build.gradle.kts
@@ -19,7 +19,6 @@ kotlin {
 
     jvm {
         java {
-            withJavadocJar()
             withSourcesJar()
         }
     }
@@ -51,38 +50,40 @@ kotlin {
     }
 }
 
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
 publishing {
-    publications {
-        create<MavenPublication>("kotlinx-gettext") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-            from(components["java"])
+    publications.withType<MavenPublication> {
+        groupId = project.group.toString()
+        version = project.version.toString()
 
-            pom {
-                name.set("Kotlinx Gettext")
-                description.set("Kotlin Gettext i18n library")
-                url.set("https://github.com/kropp/kotlinx-gettext")
+        artifact(javadocJar.get())
 
-                scm {
-                    connection.set("scm:git:git://github.com/kropp/kotlinx-gettext")
-                    developerConnection.set("scm:git:git://github.com/kropp/kotlinx-gettext")
-                    url.set("https://github.com/kropp/kotlinx-gettext/tree/main")
+        pom {
+            name.set("Kotlinx Gettext")
+            description.set("Kotlin Gettext i18n library")
+            url.set("https://github.com/kropp/kotlinx-gettext")
+
+            scm {
+                connection.set("scm:git:git://github.com/kropp/kotlinx-gettext")
+                developerConnection.set("scm:git:git://github.com/kropp/kotlinx-gettext")
+                url.set("https://github.com/kropp/kotlinx-gettext/tree/main")
+            }
+
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                 }
+            }
 
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("kropp")
-                        name.set("Victor Kropp")
-                        email.set("victor@kropp.name")
-                    }
+            developers {
+                developer {
+                    id.set("kropp")
+                    name.set("Victor Kropp")
+                    email.set("victor@kropp.name")
                 }
             }
         }
@@ -98,5 +99,5 @@ publishing {
 
 signing {
     useGpgCmd()
-    sign(publishing.publications["kotlinx-gettext"])
+    sign(publishing.publications)
 }
